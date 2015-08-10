843dd0cc3203ba2a7dd938f90c8a8cc428fa3a5d
youngKook <youngKook1992@163.com>
2015-08-10 17:07:16 +0800

    Initial Commit

---
+++ YKPageController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YKPageController
+//
+//  Created by C on 15/8/10.
+//  Copyright (c) 2015年 YoungKook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
