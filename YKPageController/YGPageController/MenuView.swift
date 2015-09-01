2c416672183bad8b120433bde42ce3fb1a69f09f
youngKook <youngKook1992@163.com>
2015-09-01 09:18:33 +0800

    修复界面少得情况下第一个按钮编程省略号的问题

---
+++ YKPageController/YGPageController/MenuView.swift
@@ -0,0 +1,285 @@
+//
+//  MenuView.swift
+//  YGPageControl
+//
+//  Created by C on 15/8/10.
+//  Copyright (c) 2015年 YoungKook. All rights reserved.
+//
+
+import UIKit
+
+protocol MenuViewDelegate {
+  func menuViewBtnClickScrollToView(index: Int)
+}
+
+let MARGIN: CGFloat = 8
+public class MenuView: UIView, UIScrollViewDelegate {
+  
+  private var titleColor: UIColor = UIColor.blackColor()
+  private var titleSelectedColor: UIColor = UIColor.orangeColor()
+  private var titlefont: CGFloat!
+  private var margin: CGFloat = 8
+  private lazy var titles = [String]()
+  private var scrollView: UIScrollView!
+  private var selectedBtn: CGButton = CGButton()
+  private var line: UIView!
+  private var sum: CGFloat!
+  private var isNoEnter: Bool = false // 防止多次进入layoutSubViews方法里头
+  var delegate: MenuViewDelegate!
+  var menuViewStyle: MenuViewStyle = .Default
+  
+  public convenience init(frame: CGRect,
+    andTitles title: [String],
+    andBackgroundcolor backgroundcolor: UIColor,
+    andTitleColor TitleColor: UIColor,
+    andTilteSelectedColor TitleSelectedColor: UIColor,
+    andTitleFont titleFont: CGFloat,
+    andStyle style: MenuViewStyle) {
+      self.init(frame: frame)
+      self.titles = title
+      self.backgroundColor = backgroundcolor
+      titleColor = TitleColor
+      titleSelectedColor = TitleSelectedColor
+      titlefont = titleFont
+      self.menuViewStyle = style
+      configScrollView()
+  }
+  
+  public override func drawRect(rect: CGRect) {
+    super.drawRect(rect)
+    if self.menuViewStyle == .Line {
+      addLineView()
+    } else {
+      
+    }
+  }
+  private func configScrollView() {
+    scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+    scrollView.showsHorizontalScrollIndicator = false
+    scrollView.showsVerticalScrollIndicator = false
+    scrollView.bounces = false
+    scrollView.delegate = self
+    addSubview(scrollView)
+    configCGButton()
+  }
+  private func configCGButton(){
+    for (i,_) in enumerate(titles) {
+      let btn = CGButton(titles: titles, andIndex: i)
+      btn.tag = i + 100
+      btn.titleLabel?.font = fontWithCGFloat(titlefont)
+      btn.selectedColor = SELECTEDCOLOR
+      btn.normalColor = NORMALCOLOR
+      btn.addTarget(self, action: Selector("btnClick:"), forControlEvents: .TouchUpInside)
+      btn.titleLabel?.textColor = NORMALCOLOR
+      scrollView.addSubview(btn)
+    }
+  }
+  public override func layoutSubviews() {
+    if isNoEnter == true { return }
+    CGLog("nimabi")
+    var btnX: CGFloat = 0 ,btnY: CGFloat ,btnW: CGFloat ,btnH: CGFloat, sumWidth: CGFloat = 0
+    var btnPre: CGButton = CGButton()
+    for (i, _) in enumerate(scrollView.subviews) {
+      let btn = scrollView.subviews[i] as! CGButton
+      if i >= 1 {
+        btnPre = scrollView.subviews[i - 1] as! CGButton
+      }
+      btnW = (btn.titleLabel?.text)!.sizeWithFonts(fontWithCGFloat(titlefont)).width
+      btnY = 0
+      btnX = btnPre.frame.origin.x + btnPre.frame.size.width + margin
+      btnH = scrollView.frame.height
+      btn.frame = CGRect(x: btnX, y: btnY, width: btnW, height: btnH)
+      sumWidth += btnW
+      if let btn = scrollView.subviews.last as? CGButton {
+        scrollView.contentSize = CGSize(width: btnX + btnW + margin, height: 0)
+      }
+      sum = sumWidth
+      if 0 == i {
+        btn.selected = true
+        selectedBtn = btn
+        if menuViewStyle == .Default {
+          btn.selectedItemBecomeBigger()
+        }
+      }
+    }
+    if scrollView.contentSize.width < self.frame.width {
+      //如果可滚动去小于menuview的宽度 重新计算MARGIN
+      margin = (self.frame.width - sumWidth) / CGFloat(scrollView.subviews.count + 1)
+      for (i, _) in enumerate(scrollView.subviews) {
+        var btn = scrollView.subviews[i] as! CGButton
+        var btnPre = CGButton()
+        if i >= 1 {
+          btnPre = scrollView.subviews[i - 1] as! CGButton
+        }
+        btnX = btnPre.frame.origin.x + btnPre.frame.width + margin
+        self.setNeedsLayout()
+      }
+    }
+    
+  }
+  
+  private func addLineView() {
+    let btn = scrollView.subviews.first as! CGButton
+    let lineView = LineView()
+    var lineX: CGFloat, lineY: CGFloat, lineW: CGFloat, lineH: CGFloat
+    lineView.backgroundColor = UIColor.redColor()
+    lineX = btn.frame.origin.x
+    lineY = btn.frame.height - 2
+    lineW = btn.frame.width
+    lineH = LINEHEIGHT
+    lineView.frame = CGRect(x: lineX, y: lineY, width: lineW, height: lineH)
+    line = lineView
+    scrollView.addSubview(lineView)
+  }
+  public func btnClick(btn: CGButton) {
+    isNoEnter == true
+    if selectedBtn == btn {
+      return
+    }
+    self.delegate.menuViewBtnClickScrollToView(btn.tag - 100)
+    selectedBtn.selected = false
+    btn.selected = true
+    menuButtonClickMove(btn.tag - 100)
+    
+    if menuViewStyle == .Line {
+      UIView.animateWithDuration(0.3, animations: {
+        self.line.frame.origin.x = btn.frame.origin.x
+        self.line.frame.size.width = btn.frame.size.width
+      })
+    } else {
+      selectedBtn.preItemBecomeSmaller()
+      btn.selectedItemBecomeBigger()
+    }
+    selectedBtn = btn
+    
+  }
+  private func menuButtonClickMove(index: Int) {
+    let count = scrollView.subviews.count
+    let btn = scrollView.subviews[index] as! CGButton
+    let newFrame = btn.convertRect(self.bounds, toView: nil)
+    let cc = newFrame.origin.x - self.center.x
+    let contentOffSetX = scrollView.contentOffset.x
+    if  index > count - 1 {return}
+    if scrollView.contentOffset.x + btn.frame.origin.x > self.center.x {
+      scrollView.setContentOffset(CGPoint(x: contentOffSetX + cc + btn.frame.width, y: 0), animated: true)
+    } else {
+      scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+  }
+  //MARK: PageControl调用
+  public func menuViewButtonMove(index: Int, andRate rate: CGFloat) {
+    let name = String(format: "scrollViewEndFinish%@", self)
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveIntoCenter:", name: name, object: nil)
+    
+    let page = Int(rate + 0.5)
+    let pageRate = rate - CGFloat(index)
+    let count = scrollView.subviews.count
+    if rate < 0 {return}
+    if menuViewStyle == .Default {
+      if index == count - 1 || index >= count - 1 {return}
+    } else {
+      if index == count - 2 || index >= count - 2 {return}
+    }
+    if pageRate == 0 {return}
+    selectedBtn.selected = false
+    let nextBtn = scrollView.subviews[index + 1] as! CGButton
+    let currentBtn = scrollView.subviews[index] as! CGButton
+    //        CGLog("\(currentBtn.titleLabel?.text)   \(nextBtn.titleLabel?.text)")
+    if menuViewStyle == .Line {
+      isNoEnter = true
+      var margin: CGFloat!
+      if rate < CGFloat(count - 2) {
+        if scrollView.contentSize.width < self.frame.size.width {
+          margin = (WIDTH - self.sum)/(CGFloat(scrollView.subviews.count + 1))
+          line.frame.origin.x = currentBtn.frame.origin.x + (currentBtn.frame.size.width + margin) * pageRate
+        } else {
+          margin = MARGIN
+          line.frame.origin.x = currentBtn.frame.origin.x + (currentBtn.frame.size.width + margin) * pageRate
+        }
+        line.frame.size.width = currentBtn.frame.size.width + (nextBtn.frame.size.width - currentBtn.frame.size.width) * pageRate
+        currentBtn.changeItemColorWithRate(pageRate)
+        nextBtn.changeItemColorWithRate(1 - pageRate)
+      }
+    } else {
+      currentBtn.changeItemColorWithScale(pageRate)
+      nextBtn.changeItemColorWithScale(1 - pageRate)
+    }
+    (scrollView.subviews[page] as! CGButton).selected = true
+    selectedBtn = scrollView.subviews[page] as! CGButton
+  }
+  public func moveIntoCenter(info: NSNotification) {
+    let user = info.userInfo as NSDictionary?
+    let page = user!.objectForKey("index") as! Int
+    menuButtonClickMove(page)
+  }
+  
+  deinit {
+    NSNotificationCenter.defaultCenter().removeObserver(self)
+  }
+  public func scrollViewDidScroll(scrollView: UIScrollView) {
+    if scrollView.contentOffset.x <= 0{
+      scrollView.contentOffset = CGPoint(x: 0, y: 0)
+    } else if scrollView.contentOffset.x + self.frame.width >= scrollView.contentSize.width{
+      // 如果 总偏移量大于contentsize
+      scrollView.contentOffset = CGPoint(x: scrollView.contentSize.width - self.frame.width, y: 0)
+    }
+  }
+  public func selectItemWithIndex(index: Int, andOtherIndex other: Int) {
+    selectedBtn = scrollView.subviews[index] as! CGButton
+    let otherBtn = scrollView.subviews[other] as! CGButton
+    selectedBtn.selected = true
+    otherBtn.selected = false
+    line.frame.origin.x = selectedBtn.frame.origin.x
+    line.frame.size.width = selectedBtn.frame.size.width
+    menuButtonClickMove(index)
+  }
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  public required init(coder aDecoder: NSCoder) {
+    fatalError("")
+  }
+  
+}
+
+class LineView: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  required init(coder aDecoder: NSCoder) {
+    fatalError("woca")
+  }
+  var radius: CGFloat = 2
+  override func drawRect(rect: CGRect) {
+    super.drawRect(rect)
+    let context = UIGraphicsGetCurrentContext()
+    CGContextSetLineWidth(context, 1.5)
+    let minx = CGRectGetMinX(rect), midx = CGRectGetMidX(rect), maxx = CGRectGetMaxX(rect)
+    let miny = CGRectGetMinY(rect), midy = CGRectGetMidY(rect), maxy = CGRectGetMaxY(rect)
+    CGContextMoveToPoint(context, minx, midy)
+    CGContextAddArcToPoint(context, minx, miny, midx, miny, radius)
+    CGContextAddArcToPoint(context, maxx, miny, maxx, midy, radius);
+    CGContextAddArcToPoint(context, maxx, maxy, midx, maxy, radius);
+    CGContextAddArcToPoint(context, minx, maxy, minx, midy, radius);
+    CGContextClosePath(context)
+    CGContextSetFillColorWithColor(context, UIColor.redColor().CGColor);
+    CGContextDrawPath(context,kCGPathFill);
+  }
+}
+
+extension NSString {
+  func sizeWithFont(font: UIFont, andMaxSize maxSize:CGSize) -> CGSize {
+    let atrribute = [NSFontAttributeName : font]
+    return self.boundingRectWithSize(maxSize, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: atrribute, context: nil).size
+  }
+  func sizeWithFont(font: UIFont, andMaxX maxX: CGFloat) -> CGSize {
+    let atrribute = [NSFontAttributeName : font]
+    let maxSize = CGSize(width: maxX, height: CGFloat(MAXFLOAT))
+    return self.boundingRectWithSize(maxSize, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: atrribute, context: nil).size
+  }
+  func sizeWithFonts(font: UIFont) -> CGSize {
+    return sizeWithFont(font, andMaxX: CGFloat(MAXFLOAT))
+  }
+}
+
+
---
+++ YKPageController/YGPageController/YGPageController.swift
@@ -0,0 +1,174 @@
+//
+//  YGPageController.swift
+//
+//
+//  Created by C on 15/8/10.
+//
+//
+
+import UIKit
+
+public enum MenuViewStyle: Int {
+    case Default = 0
+    case Line
+    case Bigger
+}
+
+public class YGPageController: UIViewController, UIScrollViewDelegate, NSCacheDelegate, MenuViewDelegate{
+    
+    //MARK: 公共变量门
+    public var menuHeight: CGFloat = 40
+    public var cacheCount: Int!
+    public var backGroundColor: UIColor = UIColor.whiteColor()
+    public var titleColor: UIColor = UIColor.blackColor()
+    public var titleSelectedColor: UIColor = UIColor.orangeColor()
+    public var titleFont: CGFloat = 16
+    var menuViewStyle: MenuViewStyle = .Default
+    //MARK: 私有变量门
+    private lazy var viewControllers = [AnyClass]()
+    private lazy var titles = [String]()
+    private lazy var frameDicts = [Int:CGRect]()
+    private var currentViewController: UIViewController!
+    private var scrollView: UIScrollView!
+    private var menuView: MenuView!
+    private lazy var dispalyVC = [Int:UIViewController]()
+    private var selectController: UIViewController!
+    private var selectIndex: Int = -1
+    private lazy var controllerCache: NSCache = self.lazyControllerCache()
+    private func lazyControllerCache() -> NSCache {
+        var cache = NSCache()
+        cache.delegate = self
+        if cacheCount == nil {
+            cache.countLimit = 3
+        } else {
+            cache.countLimit = cacheCount
+        }
+        return cache
+    }
+    public func loadViewControllers(vcArr: [AnyClass], andTitles titleArr:[String]) {
+        self.viewControllers = vcArr
+        self.titles = titleArr
+        
+    }
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+    }
+    public override func viewWillLayoutSubviews() {
+        menuView = MenuView(frame: CGRect(x: 0, y:NAVIHEIGHT, width: WIDTH, height: menuHeight), andTitles: titles, andBackgroundcolor: backGroundColor, andTitleColor: titleColor, andTilteSelectedColor: titleSelectedColor, andTitleFont: titleFont, andStyle: menuViewStyle)
+        menuView.delegate = self
+        view.addSubview(menuView)
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: NAVIHEIGHT + menuHeight, width: WIDTH, height: HEIGHT - menuHeight - NAVIHEIGHT))
+        view.addSubview(scrollView)
+        scrollView.delegate = self
+        scrollView.backgroundColor = UIColor.yellowColor()
+        scrollView.contentSize = CGSize(width: CGFloat(viewControllers.count) * WIDTH, height: 0)
+        scrollView.pagingEnabled = true
+        for var i = 0; i < viewControllers.count ; i++ {
+            let X = CGFloat(i) * WIDTH
+            let Y = CGFloat(0)
+            let W = WIDTH
+            let H = scrollView.frame.height
+            let rect = CGRect(x: X, y: Y, width: W, height: H)
+            frameDicts[i] = rect
+        }
+        addViewControllersAtIndex(0)
+    }
+    private func addViewControllersAtIndex(index: Int) {
+        let vcc: AnyClass = viewControllers[index]
+        let vc = vcc.new() as! UIViewController
+        self.addChildViewController(vc)
+        vc.view.frame = frameDicts[index]!
+        dispalyVC[index] = vc
+        scrollView.addSubview(vc.view)
+        selectController = vc
+    }
+    private func removeViewController(viewController: UIViewController, atIndex index: Int) {
+        viewController.view.removeFromSuperview()
+        viewController.willMoveToParentViewController(nil)
+        viewController.removeFromParentViewController()
+        dispalyVC.removeValueForKey(index)
+        if controllerCache.objectForKey(index) == nil {
+            controllerCache.setObject(viewController, forKey: index)
+        } else {
+            return
+        }
+    }
+    private func addCacheViewController(viewcontroller: UIViewController, atIndex index: Int) {
+        self.addChildViewController(viewcontroller)
+        scrollView.addSubview(viewcontroller.view)
+        dispalyVC.updateValue(viewcontroller, forKey: index)
+        selectController = viewcontroller
+    }
+    private func isInScreen(frame: CGRect) -> Bool {
+        let x = frame.origin.x
+        let width = scrollView.frame.size.width
+        let contentoffsetX = scrollView.contentOffset.x
+        if CGRectGetMaxX(frame) > contentoffsetX && x - contentoffsetX < width {
+            return true
+        } else {
+            return false
+        }
+    }
+    //MARK: menuViewDelegate
+    public func menuViewBtnClickScrollToView(index: Int) {
+        removeViewController(selectController, atIndex: selectIndex)
+        selectIndex = index
+        var vc = dispalyVC[index] as UIViewController?
+        if vc == nil {
+            vc = controllerCache.objectForKey(index) as? UIViewController
+            if vc != nil {
+                addCacheViewController(vc!, atIndex: index)
+            } else {
+                addViewControllersAtIndex(index)
+            }
+        }
+        scrollView.setContentOffset(CGPoint(x: CGFloat(index) * scrollView.frame.width, y: 0), animated: false)
+    }
+    //MARK: ScrollViewDelegate
+    public func scrollViewDidScroll(scrollView: UIScrollView) {
+        let page = Int(scrollView.contentOffset.x / scrollView.frame.width)
+        let rate = scrollView.contentOffset.x / scrollView.frame.width
+      for (i , value) in enumerate(viewControllers) {
+        let frame = frameDicts[i]
+        var vc: AnyObject? = dispalyVC[i]
+        if isInScreen(frame!) {
+          if (vc == nil) {
+            //从缓存中取vc
+            vc = controllerCache.objectForKey(i)
+            if vc != nil {//如果缓存中有值添加缓存里的vc
+              addCacheViewController(vc as! UIViewController, atIndex: i)
+            } else {
+              addViewControllersAtIndex(i)
+            }
+          }
+        } else {
+          if vc != nil {
+            removeViewController(vc as! UIViewController, atIndex: i)
+          }
+        }
+
+      }
+        selectController = dispalyVC[page]
+        menuView.menuViewButtonMove(page, andRate: rate)
+    }
+    public func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if scrollView.contentOffset.x < 0 || scrollView.contentOffset.x > scrollView.contentSize.width {return}
+        let page = Int(scrollView.contentOffset.x / scrollView.frame.width)
+        let name = String(format: "scrollViewEndFinish%@", menuView)
+        NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil, userInfo: ["index":page])
+    }
+    public func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.x < 0 || scrollView.contentOffset.x + WIDTH >= scrollView.contentSize.width {return}
+    }
+    public override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+    //MARK: NSCacheDelegate
+    public func cache(cache: NSCache, willEvictObject obj: AnyObject) {
+        //        CGLog(obj)
+    }
+    
+}
